a07f1b72852436fc8a9d9fe11440714135425d72
Alfredo Mariño <Alfredo Mario@Alfredo>
2017-03-06 23:35:44 -0430

    reporte 1 y dos listos

--- src/sgiir/Vistas/frameMenu.java
+++ src/sgiir/Vistas/frameMenu.java
@@ -40,7 +40,7 @@ public class frameMenu extends javax.swing.JFrame {
     
     //PANELES
     //public panelResumen VistaResumen = new panelResumen();
-    public panelBitacora VistaResumen = new panelBitacora();
+    public panelResumen VistaResumen = new panelResumen();
     public panelInstitucion VistaInstitucion = new panelInstitucion();
     public panelArea VistaArea = new panelArea();
     public panelCargo VistaCargo = new panelCargo();
